4065d0fe95e1cc7516cae781a015460b1a427ac8
LucasHenriqueSL <lucashenrique8736@gmail.com>
2022-09-21 12:04:51 -0300

    Project created 1.0

---
+++ API/Controllers/UsuarioController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Models.Usuarios;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+
+    /// <summary>
+    /// test
+    /// </summary>
+    /// <param name= "loginViewModelInput"></param>
+    ///<returns></returns>
+    [Route("api/v1/usuario")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpPost]
+        [Route("logar")]
+        public IActionResult Logar(LoginViewModelInput loginViewModelInput)
+        {
+            return Ok(loginViewModelInput);
+        }
+
+        [HttpPost]
+        [Route("registrar")]
+        public IActionResult Registrar(RegistroViewModelInput loginViewModelInput)
+        {
+            return Created("", loginViewModelInput);
+        }
+
+    }
+}
