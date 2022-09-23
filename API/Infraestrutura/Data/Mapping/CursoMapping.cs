4bd38a331b15c43a21c718cef907fa5d0669049c
LucasHenriqueSL <lucashenrique8736@gmail.com>
2022-09-23 12:48:33 -0300

    Security changes 2.0

---
+++ API/Infraestrutura/Data/Mapping/CursoMapping.cs
@@ -0,0 +1,22 @@
+﻿using API.Business.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Infraestrutura.Data.Mapping
+{
+    public class CursoMapping : IEntityTypeConfiguration<Curso>
+    {
+        public void Configure(EntityTypeBuilder<Curso> builder)
+        {
+            builder.ToTable("TB_CURSO");
+            builder.HasKey(P => P.Codigo);
+            builder.Property(P => P.Codigo).ValueGeneratedOnAdd();
+            builder.Property(P => P.Nome);
+            builder.Property(P => P.Descricao);
+        }
+    }
+}
