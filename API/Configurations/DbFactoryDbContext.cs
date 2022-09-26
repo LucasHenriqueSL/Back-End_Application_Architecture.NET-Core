68c05416f25b0bba3423ec07cb51f89b8b5edaea
LucasHenriqueSL <lucashenrique8736@gmail.com>
2022-09-26 09:33:17 -0300

    Updates

--- API/Configurations/DbFactoryDbContext.cs
+++ API/Configurations/DbFactoryDbContext.cs
@@ -1,11 +1,22 @@
-﻿using System;
+﻿using API.Infraestrutura.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace API.Configurations
 {
-    public class DbFactoryDbContext
+    public class DbFactoryDbContext : IDesignTimeDbContextFactory<CursoDBContext>
     {
+        public CursoDBContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<CursoDBContext>();
+            optionsBuilder.UseSqlServer("Server=localhost;Database=???;user=root;password=123456");
+            CursoDBContext contexto = new CursoDBContext(optionsBuilder.Options);
+
+            return contexto;
+        }
     }
 }
